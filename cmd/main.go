df150918f60a74fe6d015ace8b8648ffca781271
esferadigital <esferadigital@proton.me>
2025-08-19 11:19:08 -0500

    better welcome message

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/esferachill/mini/internal/platform"
@@ -11,7 +12,8 @@ func main() {
 	platform := platform.GetPlatform()
 	server := server.NewServer(platform.Env.Port)
 
-	log.Println("Starting Mini URL Shortener on :8080")
+	welcome := fmt.Sprintf("Starting mini at %s://%s:%d", platform.Env.HostScheme, platform.Env.Host, platform.Env.Port)
+	log.Println(welcome)
 	if err := server.Start(); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
